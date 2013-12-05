f0019967110c31f550ddda309f11a53e1c2da873
Jabara <jabaraster@gmail.com>
2013-12-05 10:01:27 +0900

    mainte.

--- modules/people/manifests/jabaraster.pp
+++ modules/people/manifests/jabaraster.pp
@@ -2,4 +2,11 @@ class people::jabaraster {
     include mou
     include virtualbox
     include vagrant
+    include heroku
+    include java
+
+    package { 'GrandPerspective':
+        source   => 'http://sourceforge.net/projects/grandperspectiv/files/grandperspective/1.5.1/GrandPerspective-1_5_1.dmg',
+        provider => appdmg,
+    }
 }
